144831741bb358f010c7dba4750b7a02e41d8f02
Lawal Rashidi <lawalrashidi95@gmail.com>
2024-05-12 16:01:15 +0100

    upadated

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import Link from "next/link";
-import { Home } from "lucide-react";
+import { Home, Palette, User, Phone, Github, Linkedin, Twitter, NotebookText } from "lucide-react";
 
 const getIcon = (icon) => {
   switch (icon) {
     case "home":
       return <Home className="w-full h-auto" strokeWidth={1.5} />;
-    case "home":
-      return <Home className="w-full h-auto" strokeWidth={1.5} />;
-    case "home":
-      return <Home className="w-full h-auto" strokeWidth={1.5} />;
-    case "home":
-      return <Home className="w-full h-auto" strokeWidth={1.5} />;
-    case "home":
-      return <Home className="w-full h-auto" strokeWidth={1.5} />;
-    case "home":
-      return <Home className="w-full h-auto" strokeWidth={1.5} />;
-    case "home":
-      return <Home className="w-full h-auto" strokeWidth={1.5} />;
+    case "about":
+      return <User className="w-full h-auto" strokeWidth={1.5} />;
+    case "projects":
+      return <Palette className="w-full h-auto" strokeWidth={1.5} />;
+    case "contact":
+      return <Phone className="w-full h-auto" strokeWidth={1.5} />;
+    case "github":
+      return <Github className="w-full h-auto" strokeWidth={1.5} />;
+    case "linkedin":
+      return <Linkedin className="w-full h-auto" strokeWidth={1.5} />;
+    case "twitter":
+        return <Twitter className="w-full h-auto" strokeWidth={1.5} />;
+    case "resume":
+        return <NotebookText className="w-full h-auto" strokeWidth={1.5} />;
     default:
       return <Home className="w-full h-auto" strokeWidth={1.5} />;
   }
@@ -32,11 +34,11 @@ const NavButton = ({ x, y, label, link, icon, newTab }) => {
       <Link
         href={link}
         target={newTab ? "_blank" : "_self"}
-        className="flex items-center justify-center text-foreground rounded-full"
+        className="flex items-center justify-center text-foreground rounded-full bg-background/20 border border-accent/30 border-solid backdrop-blur-[6px] shadow-glass-inset hover:shadow-glass-sm"
         aria-label={label}
         name={label}
       >
-        <span className="w-14 h-14 relative p-4">{getIcon(icon)}</span>
+        <span className="w-14 h-14 relative p-4 animate-spin-slow-reverse">{getIcon(icon)}</span>
       </Link>
     </div>
   );
--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
     const angleIncrement = 360/BtnList.length;
   return (
     <div className='fixed h-screen flex items-center justify-center w-full'>
-        <div className='flex items-center justify-between relative'>
+        <div className='flex items-center justify-between relative animate-spin-slow'>
         {
             BtnList.map((btn, index) => {
                 const angleRad = (index*angleIncrement*Math.PI)/180;
