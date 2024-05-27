eb7119c5db5f15ccb4c5aca3356ec2a94f8bd4aa
Lawal Rashidi <lawalrashidi95@gmail.com>
2024-05-27 18:08:53 +0100

    upadated

--- src/app/(subpages)/projects/page.js
+++ src/app/(subpages)/projects/page.js
@@ -12,7 +12,7 @@ export default function Home() {
      <Image src={bg} alt="background-image" className="-z-50 top-0 left-0 fixed w-full h-full object-cover object-center opacity-25"/>
 
      <ProjectList projects={projectsData}/>
-     <div className="flex items-center justify-end fixed top-20 -left-24 h-screen">
+     <div className="flex items-center justify-end fixed top-16 lg:top-20 -translate-x-1/2 lg:translate-x-0 left-1/2 lg:-left-24 h-screen -z-10">
      <RenderModel>
         <Staff />
       </RenderModel>
--- src/components/projects/ProjectLayout.jsx
+++ src/components/projects/ProjectLayout.jsx
@@ -6,12 +6,12 @@ const ProjectLayout = ({name, description, date , demolink}) => {
     <Link 
     href={`${demolink}`}
     target={'_blank'}
-    className='flex items-center justify-between w-full rounded-lg relative overflow-hidden p-6 custom-bg'>
+    className='text-sm md:text-base flex items-center justify-between w-full rounded-lg relative overflow-hidden p-4 md:p-6 custom-bg'>
         <div className='flex items-center justify-center space-x-2'>
             <h2 className='text-foreground'>
                 {name}
             </h2>
-            <p className='text-muted'>
+            <p className="text-muted hidden sm:inline-block">
                 {description}
             </p>
         </div>
