4eca1a9da8fba083fa27d9979963e636c6ac0f89
Lawal Rashidi <lawalrashidi95@gmail.com>
2024-06-03 15:35:52 +0100

    upadated

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -1,15 +1,36 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Volume2, VolumeX } from "lucide-react";
 
 const Sound = () => {
     const [isPlaying, setIsPlaying] = useState(false);
     const audioRef = useRef(null)
+
+    const handlerFirstUserInteraction = () => {
+        const musicConsent = localStorage.getItem("musicConsent");
+        if(musicConsent === "true" && !isPlaying){
+            audioRef.current.play()
+            setIsPlaying(true)
+        };
+        ["click", "keydown", "touchstart"].forEach((event) => document.removeEventListener(event, handlerFirstUserInteraction))
+    }
+
+    useEffect(() => {
+        const consent = localStorage.getItem("musicConsent");
+      if(consent){
+        setIsPlaying(consent === "true");
+        if(consent === "true"){
+            ["click", "keydown", "touchstart"].forEach((event) => document.addEventListener(event, handlerFirstUserInteraction ))
+        }
+      }
+    }, [])
+    
+
     const playing = () => {
         const newState = !isPlaying;
         setIsPlaying(!isPlaying);
-        !isPlaying ? audioRef.current.play() : audioRef.current.pause();
+        newState ? audioRef.current.play() : audioRef.current.pause();
         localStorage.setItem("musicConsent", String(!isPlaying))
     }
   return (
@@ -23,6 +44,7 @@ const Sound = () => {
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
         transition={{ delay: 1 }}
+        href={"/"}
         className="w-10 h-10 xs:w-14 xs:h-14 text-foreground rounded-full flex items-center justify-center cursor-pointer z-50 p-2.5 xs:p-4 custom-bg"
         aria-label={"home"}
         name={"home"}
