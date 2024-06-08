5795695ee4fc3f3c52a7b2324a9dc5cc3ec39af4
Lawal Rashidi <lawalrashidi95@gmail.com>
2024-06-08 12:15:34 +0100

    upadated

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" >
       <body className={clsx(inter.variable, "bg-background text-foreground font-inter", )}>
         {children}
         <FireFliesBackgroud />
--- src/components/FireFliesBackgroud.jsx
+++ src/components/FireFliesBackgroud.jsx
@@ -25,11 +25,11 @@ const FireFliesBackgroud = () => {
   }, []);
 
   return (
-    <div className='fixed top-0 left-0 w-full h-full -z-10 overflow-hidden'>
+    <div className="fixed top-0 left-0 w-full h-full -z-10 overflow-hidden" suppressHydrationWarning={true}>
       {
         fireFlies.map((fireFly) => {
           return <div key={fireFly.id}
-          className='absolute rounded-full w-[10px] h-[10px] bg-firefly-radial'
+          className="absolute rounded-full w-[10px] h-[10px] bg-firefly-radial"
           style={{
             top: fireFly.top,
             left: fireFly.left,
